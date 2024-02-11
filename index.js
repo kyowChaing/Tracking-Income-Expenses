2f9c84841174ece22e83bc7645248edd117cb950
Kyow <chakkek15@gmail.com>
2024-02-11 19:05:30 +0600

    using Date() constructor function and toLocalTimeStrin() and split() methode set local time formate

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-//take data from user by input form
-//select form by id and add submit Evenlistener and off  default form behaviour
 
 document.querySelector('#ewallet-form').addEventListener('submit',function(e){
     e.preventDefault();
    //taking a data from input field by quary selector and value property
-
     const type = document.querySelector('.add__type').value;
     const desc=document.querySelector('.add__description').value;
     const value=document.querySelector('.add__value').value;
-    if (desc&&value) {  //if user input value then ittems will be add
-        addItems( type,desc,value); //calling addItems() to add Items that user inputed by sendin user inputed data [type,desc,value]
-        resetForm(); //calling resetForm() to reset input field by clearing input data
+    if (desc&&value) {                                       //if user input value then ittems will be add
+        addItems( type,desc,value);                          //calling addItems() to add Items that user inputed by sendin user inputed data [type,desc,value]
+        resetForm();                                         //calling resetForm() to reset input field by clearing input data
     }
 });
 
+//adding user input data into new html element
 function addItems(type,desc,value){
-    //ceate html element block where can show user input data as a record and insert dynamically 
-    //user input data according user input
-    //using turnary operator chage coler red or green by 
-    //if type positive amount then apply income-amount style otherwisw expense-amount
+  const time = getFormattedTime() //it return current time formate
+    //ceate html element block 
     const newHtmlElementBlock = `
     <div class="item">
     <div class="item-description-time">
@@ -26,29 +22,42 @@ function addItems(type,desc,value){
         <p>${desc}</p>
       </div>
       <div class="item-time">
-        <p>25 Feb, 06:45 PM</p>
+        <p>${time}</p>
       </div>
     </div>
-    
     <div class="item-amount ${type==='+'? "income-amount":"expense-amount"}">
       <p> ${type}$ ${value}</p>
     </div>
   </div>
   </div>
 `
-
-    // console.log(newHtmlElementBlock);
-    //add newHtmlblock element into collection by using insertAdjacentHTML()
-    //here afterbein proper use to to isert starting of element in selected (innere)
+    //add newHtmlblock element into collection div by using insertAdjacentHTML()
     const collection=document.querySelector('.collection');
-    collection.insertAdjacentHTML('afterbegin',newHtmlElementBlock);
+    collection.insertAdjacentHTML('afterbegin',newHtmlElementBlock);  //here afterbein proper use to to isert starting of element in selected (innere)
 }
 
+//using curren date and time by Date() constructor function
+function getFormattedTime() {
+  const dateTime = new Date().toLocaleTimeString('en-us', {
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
 
-//creating resetForm() to reset input form by selecting input element type and 
-//setting value as empty string
+  const date=dateTime.split(',')[0].split(' ');
+  const time = dateTime.split(',')[1];
+  return `${date[1]} ${date[0]},${time}`;
+}
+
+//creating resetForm() to reset input form by selecting input element type and setting value as empty string
 function resetForm(){
     document.querySelector('.add__type').value='+';
     document.querySelector('.add__description').value='';
     document.querySelector('.add__value').value='';
 }
+
+
+// Store user inputed data into local storage as joson format as key value pair and render it from local storage
+
+
